d58482f07b917d147a95572704e18c5efc0d0d55
sudipta1411 <sdptroy3@gmail.com>
2016-10-31 21:54:03 +0530

    insert fixed

--- avl.py
+++ avl.py
@@ -11,91 +11,97 @@ class AVLTree(object) :
     def __init__(self) :
         self.root = None
 
-     def insert(self, key) :
-        self._insert(self.root, key)
+    def insert(self, key) :
+        if self.root is None :
+            self.root = Node(key)
+        else :
+            self.root = self._insert(self.root, key)
 
-    def _insert(self, key) :
-        # if self.root is None :
-        #    print '1'
-        #    self.root = Node(key)
-        # elif node is None :
+    def _insert(self, node, key) :
         if node is None :
             print '2'
             node = Node(key)
         elif key < node.key :
             print '3'
-            self.insert(key, node.left)
+            node.left = self._insert(node.left, key)
             node = self.left_rebalance(node)
         elif key > node.key :
             print '4'
-            self.insert(key, node.right)
+            node.right = self._insert(node.right, key)
             node = self.right_rebalance(node)
         else :
             print "Key already exists"
-        # return node
+        return node
 
-    def right_rebalance(self,node) :
-        l = node.left, r = node.right
-        h_l = l.height, h_r = r.height
+    def right_rebalance(self, node) :
+        l = node.left
+        r = node.right
+        h_l = self.get_height(l)
+        h_r = self.get_height(r)
         if h_r > h_l+1 :
-            if r.right.height >= r.left.height :
+            if self.get_height(r.right) >= self.get_height(r.left) :
                 node = self.rotate_left(node)
             else :
-                node = self.rotate_right_left(n)
+                node = self.rotate_right_left(node)
         else :
-            node = fix_height(node)
+            node = self.fix_height(node)
         return node
 
-    def left_rebalance(node) :
-        l = node.left, r = node.right
-        h_l = l.height, h_r = r.height
+    def left_rebalance(self, node) :
+        l = node.left
+        r = node.right
+        h_l = self.get_height(l)
+        h_r = self.get_height(r)
         if h_l > h_r+1 :
-            if l.left.height >= l.right.height :
-                node = rotate_right(node)
+            if self.get_height(l.left) >= self.get_height(l.right) :
+                node = self.rotate_right(node)
             else :
-                node = rotate_letf_right(node)
+                node = self.rotate_left_right(node)
         #else :
-        node = fix_height(node)
+        node = self.fix_height(node)
         return node
 
-    def rotate_left(node) :
+    def rotate_left(self, node) :
         t = node.right
         node.right = t.left
         t.left = node
-        fix_height(t.right)
-        fix_height(t)
+        self.fix_height(t.right)
+        self.fix_height(t)
         return t
 
-    def rotate_right(node) :
+    def rotate_right(self, node) :
         t = node.left
         node.left = t.right
         t.right = node
-        fix_height(t.right)
-        fix_height(t)
+        self.fix_height(t.right)
+        self.fix_height(t)
         return t
 
-    def rotate_letf_right(node) :
+    def rotate_left_right(self, node) :
         t = node.left
-        node.left = rotate_left(t)
-        return rotate_right(node)
+        node.left = self.rotate_left(t)
+        return self.rotate_right(node)
 
-    def rotate_right_left(node) :
+    def rotate_right_left(self, node) :
         t = node.right
-        node.right = rotate_right(t)
-        return rotate_left(node)
+        node.right = self.rotate_right(t)
+        return self.rotate_left(node)
 
-    def fix_height(node) :
+    def fix_height(self, node) :
         if node is not None :
-            h_l = node.letf.height
-            h_r = node.right.height
-            node.height = (l_h+1) if l_h > r_h else (r_h+1)
+            h_l = self.get_height(node.left)
+            h_r = self.get_height(node.right)
+            node.height = (h_l+1) if h_l > h_r else (h_r+1)
         return node
 
     def postorder(self) :
         self._postorder(self.root)
 
-    def _postorder(self,node) :
+    def _postorder(self, node) :
         if node is not None :
             self._postorder(node.left)
             self._postorder(node.right)
             print node.key
+
+    def get_height(self, node) :
+        return 0 if node is None else node.height
