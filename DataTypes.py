6c91ee0169a839ba8052499152bd28213db49ff3
sudipta1411 <sdptroy3@gmail.com>
2016-11-04 02:53:41 +0530

    New changes

--- DataTypes.py
+++ DataTypes.py
@@ -1,110 +1,252 @@
 #!/usr/bin/python
 
+from collections import namedtuple
+import math
+
 class Point(object) :
     def __init__(self, px=0.0, py=0.0) :
-        self.px = float(px)
-        self.py = float(py)
+        self._px = float(px)
+        self._py = float(py)
+
+    @property
+    def px(self) : return self._px
+
+    @property
+    def py(self) : return self._py
+
+    def dist(self,pt) : return math.sqrt((pt.px - self._px)**2 \
+            + (pt.py - self._py)**2)
 
     def __str__(self) :
-        return '({0},{1})'.format(self.px, self.py)
+        return '({0},{1})'.format(self._px, self._py)
 
 class Segment(object) :
-    # start Point, end Point
-    def __init__(self, start=None, end=None, m=None, c=None) :
-        self.start = start
-        self.end = end
-        self.m = m
-        self.c = c
+    # start Point, end Point. This is to
+    # represent line, halfline, line segment
+    def __init__(self, start=None, end=None, m=None, c=None, hl=False) :
+        self._start = start
+        self._end = end
+        self._m = m
+        self._c = c
+        self._hl = hl
 
-        if self.start and self.end :
-            self.m = (self.end.py - self.start.py) / (self.end.px - self.start.px)
-            self.c = (self.start.px * self.end.py - self.start.py * self.end.px) / (self.start.px - self.end.px)
-
-    @property
-    def m(self) : return self.m
-
-    @property.setter
-    def m(self, m) : self.m = m
-
-    @property
-    def c(self) : return self.c
-
-    @property.setter
-    def c(self, c) : self.c = c
+        if self._start and self._end :
+            if self._end.px == self._start.px :
+                self._m = self._c = float('inf')
+            else:
+                self._m = (self._end.py - self._start.py) / (self._end.px - self._start.px)
+                self._c = (self._start.px * self._end.py - self._start.py * self._end.px) \
+                        / (self._start.px - self._end.px)
 
     @property
-    def start(self) : return self.start
+    def hl(self) : return self._hl
 
-    @property.setter
-    def start(self, start) :
-        self.start = start
-        if self.start and self.end :
-            self.m = (self.end.py - self.start.py) / (self.end.px - self.start.px)
-            self.c = (self.start.px * self.end.py - self.start.py * self.end.px) / (self.start.px - self.end.px)
+    @hl.setter
+    def hl(self, val) : self._hl = val
 
     @property
-    def end(self) : return self.end
+    def m(self) : return self._m
 
-    @property.setter
-    def end(self, end) :
-        self.end = end
-        if self.start and self.end :
-            self.m = (self.end.py - self.start.py) / (self.end.px - self.start.px)
-            self.c = (self.start.px * self.end.py - self.start.py * self.end.px) / (self.start.px - self.end.px)
+    @m.setter
+    def m(self, val) : self._m = val
+
+    @property
+    def c(self) : return self._c
+
+    @c.setter
+    def c(self, val) : self._c = val
+
+    @property
+    def start(self) : return self._start
+
+    @start.setter
+    def start(self, val) :
+        self._start = val
+        if self._start and self._end :
+            if self._end.px == self._start.px :
+                self._m = self._c = float('inf')
+            else:
+                self._m = (self._end.py - self._start.py) / (self._end.px - self._start.px)
+                self._c = (self._start.px * self._end.py - self._start.py * self._end.px) \
+                        / (self._start.px - self._end.px)
+
+    @property
+    def end(self) : return self._end
+
+    @end.setter
+    def end(self, val) :
+        self._end = val
+        if self._start and self._end :
+            if self._end.px == self._start.px :
+                self._m = self._c = float('inf')
+            else:
+                self._m = (self._end.py - self._start.py) / (self._end.px - self._start.px)
+                self._c = (self._start.px * self._end.py - self._start.py * self._end.px) \
+                        / (self._start.px - self._end.px)
 
     def isOnLine(self, point) :
-        if not self.start or not self.end : return False
-        return point.py == self.m * point.px + self.c
+        if not self._start or not self._end : return False
+        return point._py == self._m * point._px + self._c
 
     def isOnSegment(self, point) :
         if not self.isOnLine(point) : return False
-        return (self.start.px<=point.px<=self.end.px) \
-                and (self.start.py<=point.py<=self.end.py)
+        return (self._start.px<=point.px<=self._end.px) \
+                and (self._start.py<=point.py<=self._end.py)
+
+    def dist(self, pt) :
+        # if self.isOnSegment(pt) :
+        return abs(self._m * pt.px - pt.py + self._c) \
+                / math.sqrt(1 + self._m**2)
+        # return min(pt.dist(self._start), pt.dist(self._end))
+
+    # XXX line is above the point
+    def above(self, pt) :
+        return pt.py < self._m * pt.px + self._c
+
+    # XXX line is below the point
+    def below(self, pt) :
+        return pt.py > self._m * pt.px + self._c
+
+    def __str__(self) : return '[{0},{1}]'.format(self._start, self._end)
 
 class Parabola(object) :
     # A parabola is determined by a focal point and directrix
+    # if p=(px,py) is the focus and L:y=mx+c is the directrix the the parabola is
+    # x^2 + y^2 -2(x*px+y*py) = delta*px^2 - m^2*delta*py^2 
+    # + delta*(c^2-2m*px*py-2c*py+2mc*px) where delta = 1/(1+m^2)
     def __init__(self, foci, direc, start=None, end=None) :
-        self.foci = foci
-        self.direc = direc
-        self.start = start
-        self.end = end
+        self._foci = foci
+        self._direc = direc
+        self._start = start
+        self._end = end
 
     @property
-    foci(self) : return self.foci
+    def foci(self) : return self._foci
 
-    @property.setter
-    def foci(self, foci) : self.foci = foci
-
-    @property
-    def direc(self) : return self.direc
-
-    @property.setter
-    def direc(self, direc) : self.direc = direc
+    @foci.setter
+    def foci(self, val) : self._foci = val
 
     @property
-    def start(self) : return self.start
+    def direc(self) : return self._direc
 
-    @property.setter
-    def start(self, start) : self.start = start
+    @direc.setter
+    def direc(self, val) : self._direc = val
 
     @property
-    def end(self) : return self.end
+    def start(self) : return self._start
 
-    @property.setter
-    def end(self, end) : self.end = end
+    @start.setter
+    def start(self, val) : self._start = val
+
+    @property
+    def end(self) : return self._end
+
+    @end.setter
+    def end(self, val) : self._end = val
 
 class VertLine(object) :
     # represent a vertical line through a given point to a given line
     # if no line is specified, then the line is vertical to x-axis
+    # pt in most cases is the endpoint of the line
     def __init__(self, pt, line=None) :
-        self.pt = pt
-        self.vert = Segment()
+        self._pt = pt
+        self._vert = Segment()
         if line is not None :
-            self.vert.m = -1.0 / line.m
-            self.vert.c = self.pt.py - self.vert.m * pt.px
+            self._vert.m = -1.0 / line.m
+            self._vert.c = self._pt.py - self._vert.m * pt.px
 
     @property
-    def pt(self) : return self.pt
+    def pt(self) : return self._pt
 
     @property
-    def vert(self) : return self.vert
+    def vert(self) : return self._vert
+
+class Site(object) :
+    # a site can be either a point or a line segment
+    def __init__(self, pt=None, seg=None) :
+        self._pt = pt
+        self._seg = seg
+
+    @property
+    def pt(self) : return self._pt
+
+    @property
+    def seg(self) : return self._seg
+
+    def isPointSite(self) : return self._pt is not None
+
+    def isLineSite(self) : return self._seg is not None
+
+class Vertex(object) :
+    # A vertex is always a point
+    def __init__(self, pt) :
+        self._pt = pt
+
+    @property
+    def pt(self) : return self._pt
+
+class Edge(object) :
+    # An edge can be either a line segment,
+    # halfline or a section of parabola
+    def __init__(self, seg=None, prb=None) :
+        self._seg = seg
+        self._prb = prb
+
+    @property
+    def seg(self) : return self._seg
+
+    @seg.setter
+    def seg(self, val) : self._seg = val
+
+    @property
+    def prb(self) : return self._prb
+
+    @prb.setter
+    def prb(self, val) : self._prb = val
+
+class Quad(object) :
+    # left and right are vertical lines upper
+    # and lower are long walls. Left and right
+    # seperators are annotated with the windows
+    # created by the crossing. Each window is a
+    # vertical line segment or half line.
+    def __init__(self, upper=None, lower=None, left=None, right=None) :
+        self._upper = upper
+        self._lower = lower
+        self._left = left
+        self._right = right
+        self._active = False
+        self.r_windows = []
+        self.l_windows = []
+
+    @property
+    def upper(self) : return self._upper
+
+    @upper.setter
+    def upper(self, val) : self._upper = val
+
+    @property
+    def lower(self) : return self._lower
+
+    @lower.setter
+    def lower(self, val) : self._lower = val
+
+    @property
+    def left(self) : return self._left
+
+    @left.setter
+    def left(self, val) : self._left = val
+
+    @property
+    def right(self) : return self._right
+
+    @right.setter
+    def right(self, val) : self._right = val
+
+    @property
+    def active(self) : return self._active
+
+    @active.setter
+    def active(self, val) : self._active = val
+
+    def getCorners(self) : pass
--- Voronoi.py
+++ Voronoi.py
@@ -1,34 +1,118 @@
 #!/usr/bin/python
 
-from priority_queue import PrioQueue
-from avl import AVLTree
+import math
+from DataTypes import *
 
-S = 'site'
-I = 'intersection'
+class EndPoint(object) :
+    def __init__(self, pt, line, left_sep=None, \
+            right_sep=None, above=None, below=None) :
+        self.pt = Site(pt=pt)
+        self.line = Site(seg=line)
+        self.left_sep = left_sep
+        self.right_sep = right_sep
+        self.above = above
+        self.below = below
+
+def avgx(ep1, ep2) :
+    return (ep1.pt.pt.px + ep2.pt.pt.px) / 2.0
 
 class Voronoi(object) :
-    '''
-    @data is a list of points(sites)
-    '''
-    def __init__(self, data) :
-        self.sites = data
-        self.result = []
-        # event points can be either sites or intersection points
-        self.events  = PrioQueue()
-        self.nr_sites = len(self.sites)
-        # binary tree to store the regions and boundary
-        self.bt = AVLTree()
+    # lines is a list of line segments,
+    # defined by start and end points
 
-        for x,y in self.sites :
-            self.events.push((x,y,S))
+    # LINE = 0; LEFT_SEP=1; RIGHT_SEP=2; ABOVE=3; BELOW=4
+    DELTA = 10.0
+
+    def __init__(self, lines) :
+        self.lines = lines
+        self.nr_pt = 2 * len(self.lines) # number of point sites
+
+        self.ep = [] # endpoints
+        for line in self.lines :
+            self.ep.append(EndPoint(pt=line.start, line=line))
+            self.ep.append(EndPoint(pt=line.end, line=line))
+        self.ep.sort(key = lambda x : x.pt.pt.px) # presort
+
+        self.sep = [] # list of (nr_pt+1) seperators
+        self.sep.append(VertLine(pt = Point(px = self.ep[0].pt.pt.px - Voronoi.DELTA)))
+        for i in range(1, self.nr_pt) :
+            self.ep[i-1].left_sep = self.sep[i-1]
+            self.sep.append(VertLine(pt = avgx(self.ep[i-1], self.ep[i])))
+            self.ep[i-1].right_sep = self.sep[i]
+        self.ep[self.nr_pt-1].left_sep = self.sep[self.nr_pt-1]
+        self.sep.append(VertLine(pt = Point(px = self.ep[self.nr_pt-1].pt.pt.px + Voronoi.DELTA)))
+        self.ep[self.nr_pt-1].right_sep = self.sep[-1]
+
+        self._vertical_ray_shoot() # to populate the above and below field
+
+        self.output = [] # list of Vorornoi Edges
 
     def build(self) :
-        while not self.events.empty() :
-            p = self.events.pop()
-            if p[2] == S :
-                selft.process_site(p)
-            else :
-                self.process_intersection(p)
+        print '[INFO] Building Voronoi Diagram...'
+        m = self.nr_pt
+        self._build(m, 0, self.nr_pt) # slab is [sep[0], sep[nr_pt]]
 
-    def process_site(self, point) :
+    # returns tuple tup where tup[0] is a list of quads
+    # in the current slab, where each slab is marked
+    # active/inactive along with their windows, tup[1] is
+    # a list of Voronoi edges for these quads and tup[2]
+    # is the Convex Hull of the sites in the current slab.
+    # When the recursive call end in method @build, we have
+    # the entire diagram in @output.
+    def _build(self, m, l_id, r_id) :
+        # Basic error checking
+        if l_id < 0 :
+            raise ValueError("Invalid left seperator index = {0}".format(l_id))
+        if r_id > self.nr_pt :
+            raise ValueError("Invalid right seperator index = {0}".format(r_id))
+        if r_id < l_id :
+            raise ValueError("right seperator({0}) is to the left of left seperator({1})"
+                    .format(r_id,l_id))
+
+        if m == 1:
+            # base case
+            return
+        # Divide
+        m_ceil = int(math.ceil(m / 2.0))
+        m_floor = int(math.floor(m / 2.0))
+        self._build(m_ceil, l_id, l_id + m_ceil)
+        self._build(m_floor, l_id + m_ceil, r_id)
+
+        #conquer
+
+    # XXX not very efficient
+    def _vertical_ray_shoot(self) :
+        for end_point in self.ep :
+            point = end_point.pt.pt
+            candidate = []
+            for line in self.lines :
+                if line.start.px < point.px and line.end.px > point.px :
+                    candidate.append(line)
+            d_min_above = float('inf')
+            d_min_below = float('inf')
+            for cand in candidate :
+                d = cand.dist(point)
+                if cand.above(point) :
+                    if d < d_min_above :
+                        end_point.above = cand
+                        d_min_above = d
+                else :
+                    if cand.dist(point) < d_min_below :
+                        end_point.below = cand
+                        d_min_below = d
+
+# TEST
+def main() :
+    s1 = Segment(Point(6,5), Point(7,8))
+    s2 = Segment(Point(8,4), Point(10,16))
+    s3 = Segment(Point(2,8), Point(15,10))
+
+    lines = [s1,s2,s3]
+
+    vor = Voronoi(lines)
+    vor.build()
+
+if __name__ == '__main__' :
+    main()
+
 
